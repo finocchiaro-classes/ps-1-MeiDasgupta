38403c057922183b13925af5d7025096b7d31319
MeiDasgupta <dasgupt@bc.edu>
2025-09-02 17:56:36 -0400

    Update ps1-part1.py

--- ps1-part1.py
+++ ps1-part1.py
@@ -1,10 +1,11 @@
-# Ask the user for the first number, store the value in a variable
-
-
-# Ask the user for the second number, store the value in a variable
-
-
-# Repeat back the numbers
-
-
-# Perform calculations. Be careful about string formatting for autograders.
+firstnum = int(input("Enter an integer between 10 and 100:"))
+secondnum = int(input("Enter an integer less than 4:"))
+print("You entered " + str(firstnum) +  " and " + str(secondnum))
+sum_of_first_and_second_num = firstnum + secondnum
+product_of_first_and_second_num = firstnum * secondnum
+firstnum_raised_to_secondnum_power = firstnum ** secondnum
+remainder = firstnum % secondnum
+print(str(firstnum) + " + " + str(secondnum) + " = " + str(sum_of_first_and_second_num))
+print(str(firstnum) + " * " + str(secondnum) + " = " + str(product_of_first_and_second_num))
+print(str(firstnum) + " ** " + str(secondnum) + " = " + str(firstnum_raised_to_secondnum_power))
+print(str(firstnum) + " % " + str(secondnum) + " = " + str(remainder))
