990868635bc3626da0e04783cf988449d96af850
MeiDasgupta <dasgupt@bc.edu>
2025-09-08 01:58:28 -0400

    Update ps1-part3.py

--- ps1-part3.py
+++ ps1-part3.py
@@ -1,1 +1,19 @@
+def heart_rate(age, goal):
+    max_HR = 220 - age
+    print("Your maximum heart rate is:", max_HR)
+    
+    if goal == "S":
+        low_target_HR = .8 * max_HR
+        high_target_HR = max_HR
+        print("Your target heart rate is between", low_target_HR, "and", high_target_HR)
 
+    if goal == "E":
+        low_target_HR = .6 * max_HR
+        high_target_HR = .8 * max_HR
+        print("Your target heart rate is between", low_target_HR, "and", high_target_HR)
+
+
+user_age = int(input("What is your age? "))
+user_goal = input("Is your goal speed (S) or endurance (E)? ")
+
+heart_rate(user_age, user_goal)
