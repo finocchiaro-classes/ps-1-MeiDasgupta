0ef8ced832f1afa69f9369092f2bdf9247e149ba
MeiDasgupta <dasgupt@bc.edu>
2025-09-08 01:27:04 -0400

    Update ps1-part2.py

--- ps1-part2.py
+++ ps1-part2.py
@@ -1,11 +1,10 @@
-# Write a function that takes two variables and does all the computations asked
+def number_fun(a, b):
+    print("You entered:", a, "and", b)
+    print(a + b)
+    print(a * b)
+    print(a ** b)
+    print(a % b)
+firstnum = int(input("Provide an integer between 10 and 100"))
+secondnum = int(input("Provide an integer less than 4"))
+number_fun(firstnum, secondnum)
 
-# Ask the user for the first number, store the value in a variable
-
-
-# Ask the user for the second number, store the value in a variable
-
-
-# Repeat back the numbers
-
-# Perform calculations. Be careful about string formatting for autograders.
